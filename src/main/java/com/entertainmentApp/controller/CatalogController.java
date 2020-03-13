715e5016cd88b3b040a7374f19356dd448164255
VladLishen <vlad05ik1485@gmail.com>
2020-03-13 13:54:55 +0200

    admin link

--- src/main/java/com/entertainmentApp/controller/CatalogController.java
+++ src/main/java/com/entertainmentApp/controller/CatalogController.java
@@ -3,6 +3,7 @@ package com.entertainmentApp.controller;
 import com.entertainmentApp.domain.Entertainment;
 import com.entertainmentApp.service.EntertainmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,6 +25,7 @@ public class CatalogController {
 
     /**
      * This is method display catalog.
+     * And returns a service link if the role is ADMIN.
      * @param model
      * @return catalog
      */
@@ -31,6 +33,10 @@ public class CatalogController {
     public String viewCatalog(final Model model) {
         Iterable<Entertainment> entertainments = entertainmentService.findAll();
         model.addAttribute("entertainments", entertainments);
+        String author = SecurityContextHolder.getContext().getAuthentication().getAuthorities().toString();
+        if (!author.equals(null)) {
+            model.addAttribute("author", author);
+        }
         return "catalog";
     }
 
